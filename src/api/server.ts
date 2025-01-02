1096091e23d8b83da17f0a496ca9eb278ca4d760
Trevor Farias <levelonethousandr@gmail.com>
2025-01-01 17:45:30 -0800

    good to go hopefully

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,8 +1,26 @@
-const express = require("express");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
+
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "../../src/index.html"));
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+httpServer.listen(port, () => {
+  console.log('listening on *:3000');
+});
 
 module.exports = app;
